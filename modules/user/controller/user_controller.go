2f9d4d62821f5ec4fc9dc45819d5e3cfdf125dca
Gerry Nicholas <gerrynicholas76@gmail.com>
2026-04-16 11:04:12 +0700

    init repo

---
+++ modules/user/controller/user_controller.go
@@ -0,0 +1,60 @@
+package controller
+
+import (
+    "net/http"
+    "strconv"
+
+    "github.com/gin-gonic/gin"
+    "github.com/Mobilizes/materi-be-alpro/modules/user/service"
+    "github.com/Mobilizes/materi-be-alpro/modules/user/validation"
+    "github.com/Mobilizes/materi-be-alpro/pkg/utils"
+)
+
+type UserController struct {
+    service *service.UserService
+}
+
+func NewUserController(service *service.UserService) *UserController {
+    return &UserController{service: service}
+}
+
+func (ctrl *UserController) CreateUser(c *gin.Context) {
+    req, err := validation.ValidateCreateUser(c)
+    if err != nil {
+        utils.ErrorResponse(c, http.StatusBadRequest, err.Error())
+        return
+    }
+
+    user, err := ctrl.service.CreateUser(req)
+    if err != nil {
+        utils.ErrorResponse(c, http.StatusInternalServerError, "Gagal membuat user")
+        return
+    }
+
+    utils.SuccessResponse(c, http.StatusCreated, "User berhasil dibuat", user)
+}
+
+func (ctrl *UserController) GetUserByID(c *gin.Context) {
+    id, err := strconv.Atoi(c.Param("id"))
+    if err != nil {
+        utils.ErrorResponse(c, http.StatusBadRequest, "ID tidak valid")
+        return
+    }
+
+    user, err := ctrl.service.GetUserByID(uint(id))
+    if err != nil {
+        utils.ErrorResponse(c, http.StatusNotFound, "User tidak ditemukan")
+        return
+    }
+
+    utils.SuccessResponse(c, http.StatusOK, "OK", user)
+}
+
+func (ctrl *UserController) GetAllUsers(c *gin.Context) {
+    users, err := ctrl.service.GetAllUsers()
+    if err != nil {
+        utils.ErrorResponse(c, http.StatusInternalServerError, "Gagal mengambil data")
+        return
+    }
+    utils.SuccessResponse(c, http.StatusOK, "OK", users)
+}
---
+++ modules/user/repository/user_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+    "github.com/Mobilizes/materi-be-alpro/database/entities"
+    "gorm.io/gorm"
+)
+
+type UserRepository struct {
+    db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+    return &UserRepository{db: db}
+}
+
+func (r *UserRepository) Create(user *entities.User) error {
+    return r.db.Create(user).Error
+}
+
+func (r *UserRepository) FindByID(id uint) (*entities.User, error) {
+    var user entities.User
+    err := r.db.First(&user, id).Error
+    return &user, err
+}
+
+func (r *UserRepository) FindAll() ([]entities.User, error) {
+    var users []entities.User
+    err := r.db.Find(&users).Error
+    return users, err
+}
+
+func (r *UserRepository) FindByEmail(email string) (*entities.User, error) {
+    var user entities.User
+    err := r.db.Where("email = ?", email).First(&user).Error
+    return &user, err
+}
